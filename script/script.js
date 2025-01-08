5667a693d988c45d54531263597ebed0c68c2383
Letícia Carpes <lecarpesoliveira@gmail.com>
2025-01-08 09:31:07 -0300

    Solucionando problema scroll menu

--- script/script.js
+++ script/script.js
@@ -1,6 +1,21 @@
 
 const nav = document.querySelector(".navHeader");
+const modal = document.getElementById("modal");
 
-document.getElementById("modal").addEventListener("click", () =>{
+
+nav.classList.remove("open");
+
+modal.addEventListener("click", () =>{
     nav.classList.toggle("open")
 });
+
+window.addEventListener("scroll", () => {
+    // Se o menu estiver aberto (a classe "open" estiver presente)
+    if (nav.classList.contains("open")) {
+        // Remover a classe "open" para fechar o menu
+        nav.classList.remove("open");
+    }
+});
+
+
+
